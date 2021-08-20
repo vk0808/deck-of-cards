3a5f7046e76bceb44f9afdaaf64ef0afacb5810c
vk0808 <vk4765712@gmail.com>
2021-08-20 18:21:29 +0530

    add: welcome message

---
+++ DeckOfCards/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Welcome message
+            Console.WriteLine("Deck of Cards\n");
+        }
+    }
+}
